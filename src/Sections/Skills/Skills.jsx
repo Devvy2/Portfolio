490220d73b12fe544114652b73576b5d65e4795a
Devvy2 <jsorenesh04.kodehode@gmail.com>
2023-09-19 10:50:52 +0200

    updated skills section and some basic styling

--- src/Sections/Skills/Skills.jsx
+++ src/Sections/Skills/Skills.jsx
@@ -3,18 +3,42 @@ import "./skills.css";
 export function Skills() {
   return (
     <div className="skills-section">
-      <h1 className="skills-title">Skills</h1>
-      <div className="skills-img-body">
-        <div className="skills-img-row1">
-          <img src="src/assets/images/html 1.png" alt="" />
-          <img src="src/assets/images/css 1.png" alt="" />
-          <img src="src/assets/images/javascript.png" alt="" />
-          <img src="src/assets/images/react.png" alt="" />
-        </div>
-        <div className="skills-img-row2">
-          <img src="src/assets/images/Figma.png" alt="" />
-          <img src="src/assets/images/github.png" alt="" />
-          <img src="src/assets/images/Visual studio code.png" alt="" />
+      <div className="skills-body">
+        <h1 className="skills-title">Skills</h1>
+        <div className="skills-img-body">
+          <div className="skills-img-row1">
+            <div>
+              <img src="src/assets/images/html 1.png" alt="" />
+              <p>HTML 5</p>
+            </div>
+            <div>
+              <img src="src/assets/images/css 1.png" alt="" />
+              <p>CSS 3</p>
+            </div>
+            <div>
+              <img src="src/assets/images/javascript.png" alt="" />
+              <p>Javascript</p>
+            </div>
+            <div>
+              <img src="src/assets/images/react.png" alt="" />
+              <p>React</p>
+            </div>
+          </div>
+
+          <div className="skills-img-row2">
+            <div>
+              <img src="src/assets/images/Figma.png" alt="" />
+              <p>Figma</p>
+            </div>
+            <div>
+              <img src="src/assets/images/github.png" alt="" />
+              <p>Github</p>
+            </div>
+            <div>
+              <img src="src/assets/images/Visual studio code.png" alt="" />
+              <p>Visual Studio</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
